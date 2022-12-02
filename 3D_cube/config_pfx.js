a425226546c3747f4a16d60046c4c471ecd20387
PerformicsTwDev <89893667+PerformicsTwDev@users.noreply.github.com>
2022-12-02 17:14:44 +0800

    v1

---
+++ 3D_cube/config_pfx.js
@@ -0,0 +1,34 @@
+var Performics = {};
+Performics.settings = {
+  Lp_url:
+    "https://www.mcdonalds.com/tw/zh-tw/whats-hot/Signature.html?utm_source=APEXPMP_MO&utm_medium=display&utm_term=LBMcD&utm_content=0915_SignatureTruffle_Display_APEXPMP_MO&utm_campaign=APEXPMP_MO_SignatureTruffle_0915",
+  exit_label: "exit",
+  video_source_url:
+    "https://gcdn.2mdn.net/videoplayback/id/0be426dd79bfc864/itag/15/source/doubleclick/ratebypass/yes/mime/video%2Fmp4/acao/yes/ip/0.0.0.0/ipbits/0/expire/3775515651/sparams/id,itag,source,ratebypass,mime,acao,ip,ipbits,expire/signature/8FDF14310D36D205A28AAC5DBF160F61DD374EEE.50E204706DBC0E050EAC5F9C41AD7E4B74C141BA/key/ck2/file/file.mp4",
+  FLD: {
+    /*FLD basic settings */
+    FLD_src: "",
+    FLD_type: "",
+    FLD_cat: "",
+
+    /*FLD u1 settings*/
+    DBM_IMP: "IMP",
+    DBM_CLK: "CLK",
+
+    /* FLD u3-u10 parameter settings*/
+    u1: "0",
+    u2: "0",
+    u3: "0",
+    u4: "0",
+    u5: "0",
+    u6: "0",
+    u7: "0",
+    u8: "0",
+    u9: "0",
+    u10: "0",
+  },
+
+  isResourceLoaded: false,
+  is_clicked: false,
+  is_video_exist: true,
+};
